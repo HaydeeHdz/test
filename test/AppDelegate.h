e846e743df1340127cfbcb0230ec56be7e0438ac
Haydee <haydee@haydees-imac.local>
2016-06-29 23:51:42 -0500

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Haydee on 6/29/16.
+//  Copyright © 2016 Haydee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Haydee on 6/29/16.
+//  Copyright © 2016 Haydee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
